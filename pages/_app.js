a5f8ee36f5ad8924226de8b3f165f07b71ee4e97
Casey Carroll <caseydean8@gmail.com>
2023-03-01 11:56:05 -0500

    Optimized font with @next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Lato } from "@next/font/google";
+
+const lato = Lato({
+  weight: ["100", "300", "400"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
