405aa7894269c04e1e9c7188aeee701f976360e6
Casey Carroll <caseydean8@gmail.com>
2023-03-11 12:36:40 -0500

    Add graphql-request module

--- services/index.js
+++ services/index.js
@@ -1,4 +1,5 @@
-import { request, gql } from "graphql-request";
+// import { request, gql } from "graphql-request";
+import { request, gql, GraphQLClient } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
