13d7d76cf6fd7d7ed2479792fa4057eb3c6de493
Casey Carroll <caseydean8@gmail.com>
2023-03-27 13:49:09 -0400

    Added language attribute to html tag for Accessibility

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 
 module.exports = {
-    images: {
-        domains: ["media.graphassets.com"]
-    }
-}
+  images: {
+    domains: ["media.graphassets.com"],
+  },
+};
