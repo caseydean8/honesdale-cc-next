e75fec9db85effa6b6bd3674a5bb1607e4362063
Casey Carroll <caseydean8@gmail.com>
2023-05-25 17:06:03 -0400

    Added favicon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-import FooterLogo from "../public/hcc-new-logo_3.webp";
 import { FaFacebookSquare, FaYoutube } from "react-icons/fa";
 import { IoMailOutline } from "react-icons/io5";
 
